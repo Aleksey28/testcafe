0a482692509c7470e1759c624db6fdf32801d7ab
Aleksey28 <alekseypopow1995@yandex.ru>
2021-06-18 17:23:56 +0300

    test: added test for global fixture hooks

--- test/functional/setup.js
+++ test/functional/setup.js
@@ -233,6 +233,7 @@ before(function () {
                     pageRequestTimeout,
                     ajaxRequestTimeout,
                     userVariables,
+                    hooks,
                 } = opts;
 
                 const actualBrowsers = browsersInfo.filter(browserInfo => {
@@ -298,6 +299,7 @@ before(function () {
                         pageRequestTimeout,
                         ajaxRequestTimeout,
                         userVariables,
+                        hooks,
                     })
                     .then(failedCount => {
                         if (customReporters)
