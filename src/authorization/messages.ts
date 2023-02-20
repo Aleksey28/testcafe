c8e2be98162b6b2db034d7f7ad56d46f7b16d3c5
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-02-20 12:58:11 +0400

    fix: fixed login messages

--- src/authorization/messages.ts
+++ src/authorization/messages.ts
@@ -1,4 +1,4 @@
 export const AUTHORIZATION_COMPLETED = 'You have successfully logged into a DevExpress account.';
 export const AUTHORIZATION_FAILED    = 'Something went wrong. Run the command again. If the problem persists, contact the TestCafe team: [EMAIL]';
-export const AUTHORIZATION_REQUEST   = `You have not logged into a DevExpress account. Do you want to log in? [Y/N] Important: After the upcoming TestCafé v3.0 update, you'll need a free DevExpress account to run the framework.`;
+export const AUTHORIZATION_REQUEST   = `You have not logged into a DevExpress account. After the upcoming TestCafé v3.0 update, you'll need a free DevExpress account to run the framework. Do you want to log in?`;
 export const NOT_AUTHORIZED          = 'You have not signed into a DevExpress account. To dismiss this message, run "testcafe login" and follow the instructions.';
