74eadeb942784fac387834b05d65aa25e0b0c5f6
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-02-21 18:58:13 +0400

    fix: added blank line to auth messages

--- src/authorization/messages.ts
+++ src/authorization/messages.ts
@@ -1,4 +1,4 @@
-export const AUTHORIZATION_COMPLETED = 'You have successfully logged into a DevExpress account.';
-export const AUTHORIZATION_FAILED    = 'Something went wrong. Run the command again. If the problem persists, contact the TestCafe team: [EMAIL]';
+export const AUTHORIZATION_COMPLETED = '\nYou have successfully logged into a DevExpress account.\n';
+export const AUTHORIZATION_FAILED    = '\nSomething went wrong. Run the command again. If the problem persists, contact the TestCafe team: [EMAIL]\n';
 export const AUTHORIZATION_REQUEST   = `You have not logged into a DevExpress account. After the upcoming TestCafé v3.0 update, you'll need a free DevExpress account to run the framework. Do you want to log in?`;
-export const NOT_AUTHORIZED          = 'You have not signed into a DevExpress account. To dismiss this message, run "testcafe login" and follow the instructions.';
+export const NOT_AUTHORIZED          = '\nYou have not signed into a DevExpress account. To dismiss this message, run "testcafe login" and follow the instructions.\n';
