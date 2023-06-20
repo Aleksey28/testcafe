fc9d30b4726023e7ebabfd96edf3948ecf0362fd
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-06-20 14:52:42 +0300

    test

--- Gulpfile.js
+++ Gulpfile.js
@@ -424,7 +424,7 @@ gulp.task('test-functional-local-chrome-firefox', gulp.series('prepare-tests', '
 gulp.step('test-functional-local-safari-run', () => {
     return testFunctional(TESTS_GLOB, functionalTestConfig.testingEnvironmentNames.localSafari)
     .then(() => {
-        process.kill(process.pid);
+        process.abort();
     })
     .catch((e) => {
         console.log(`${new Date()} -> file: Gulpfile.js:430 -> gulp.step -> e:`, e);
